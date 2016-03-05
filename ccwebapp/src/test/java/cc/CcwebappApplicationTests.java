52b8638e182dd965ed752548dd1a355bffbccf2e
qharryq <harry.quigley2@mail.dcu.ie>
2016-03-05 02:25:24 +0000

    spring boot web initializer

---
+++ ccwebapp/src/test/java/cc/CcwebappApplicationTests.java
@@ -0,0 +1,18 @@
+package cc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CcwebappApplication.class)
+@WebAppConfiguration
+public class CcwebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
