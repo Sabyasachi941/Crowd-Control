bfbf3a218fc0a8861ea5e40fe8e2ad50839eeb9c
qharryq <harry.quigley2@mail.dcu.ie>
2016-03-05 01:05:44 +0000

    spring boot web initializer

--- ccwebapp/src/main/java/crowdcontrol/CcwebappApplication.java
+++ ccwebapp/src/main/java/crowdcontrol/CcwebappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-
+@EnableAutoConfiguration
 @SpringBootApplication
 public class CcwebappApplication extends SpringBootServletInitializer {
 
