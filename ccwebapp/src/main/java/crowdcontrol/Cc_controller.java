e64f0f7916037909dd76441473d95670f1116769
qharryq <harry.quigley2@mail.dcu.ie>
2016-03-05 01:49:22 +0000

    spring boot web initializer

--- ccwebapp/src/main/java/crowdcontrol/Cc_controller.java
+++ ccwebapp/src/main/java/crowdcontrol/Cc_controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Cc_controller {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }
