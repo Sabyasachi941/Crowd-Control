345590131d4ff6b2aa68c2acfd18193597041765
qharryq <harry.quigley2@mail.dcu.ie>
2016-03-24 15:29:02 +0000

    db connection established

--- ccwebapp/src/main/java/cc/CcwebappApplication.java
+++ ccwebapp/src/main/java/cc/CcwebappApplication.java
@@ -10,15 +10,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
-
+import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextRegion;
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableJpaRepositories
+@EnableRdsInstance(databaseName="ebdb", dbInstanceIdentifier="aa26q2nxcdc5te", username="Harry",password="${spring.aws.pass}")
+@EnableContextCredentials(accessKey="AKIAJE46KTH4DUZDQMSQ", secretKey="${spring.aws.secKey}")
+@EnableContextRegion(region="eu-west-1")
+
+
+
+
 public class CcwebappApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
         SpringApplication.run(CcwebappApplication.class, args);
@@ -29,15 +41,8 @@ public class CcwebappApplication extends SpringBootServletInitializer {
         return application.sources(applicationClass);
     }
 
+
+
     private static Class<CcwebappApplication> applicationClass = CcwebappApplication.class;
 }
 
-@Controller
-class GreetingController {
-    
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-}
