dc9a9484af8f953bb021237c7de28fbb04726ff0
qharryq <harry.quigley2@mail.dcu.ie>
2016-05-01 00:22:56 +0100

    rest service consumed by angular

--- ccwebapp/src/main/resources/static/app.js
+++ ccwebapp/src/main/resources/static/app.js
@@ -3,33 +3,6 @@ var app = angular.module('app', ['restangular'])
         RestangularProvider.setBaseUrl('http://localhost:8181/');
     });
 
-
-
-    app.config(function(RestangularProvider) {
-
-        // add a response intereceptor
-        RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
-          var extractedData;
-          // .. to look for getList operations
-          if (operation === "getList") {
-            // .. and handle the data and meta data
-            resp =  data._embedded[what];
-            resp._links = data._links;
-            return resp
-
-          }
-
-          return data;
-        });
-
-        RestangularProvider.setRestangularFields({
-            selfLink: 'self.link'
-        });
-
-
-});
-
 app.controller('IndexCtrl', function($scope, Restangular) {
-        $scope.dayTotalPeoples = Restangular.all('dayTotalPeoples').getList();
-
-    });
+    $scope.graphData = Restangular.all('graphData').getList();
+});
