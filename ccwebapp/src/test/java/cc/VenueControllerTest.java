05081fb8b10a8ec1ef6d9197ceb412086ca2a8ff
qharryq <harry.quigley2@mail.dcu.ie>
2016-05-18 21:42:44 +0100

    crud tests for venue controller

--- ccwebapp/src/test/java/cc/VenueControllerTest.java
+++ ccwebapp/src/test/java/cc/VenueControllerTest.java
@@ -1,26 +1,25 @@
 package cc;
+/**
+ * Created by harryquigley on 18/05/2016.
+ */
+
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-/**
- * Created by harryquigley on 18/05/2016.
- */
 public class VenueControllerTest {
 
     @Mock //Mockito mock object
@@ -90,6 +89,56 @@ public class VenueControllerTest {
     }
 
     //need to test binding of the venue being read in to be saved and the properties of the venue which is outputted as a result
+    //Currently not passing due to id
+    @Test
+    public void testSaveVenue() throws Exception{
+        Integer id = 1;
+        String email = "example@hotmail.com";
+        Integer capacity = 100;
+        String password = "example";
 
+        //id is automatically generated - can't set it
+        Venue returnVenue = new Venue();
+        returnVenue.setEmail(email);
+        returnVenue.setCapacity(capacity);
+        returnVenue.setPassword(password);
+        //should i set timestamps also?
+
+        Mockito.when(venueService.saveVenue(Matchers.<Venue>any())).thenReturn(returnVenue);
+
+        mockMvc.perform(post("/venue")
+                .param("id","1")
+                .param("email","example@hotmail.com")
+                .param("capacity","100")
+                .param("password","example"))
+                    .andExpect(status().is3xxRedirection()) //checks if there was redirect - there should be
+                    .andExpect(view().name("redirect:/venue/10"))
+                    .andExpect(model().attribute("venue", instanceOf(Venue.class)))
+                    .andExpect(model().attribute("venue", hasProperty("id",is(id))))
+                    .andExpect(model().attribute("venue", hasProperty("email",is(email))))
+                    .andExpect(model().attribute("venue", hasProperty("capacity",is(capacity))))
+                    .andExpect(model().attribute("venue", hasProperty("password",is(password))));
+
+        //verify properties of bound Venue
+        ArgumentCaptor<Venue> boundVenue = ArgumentCaptor.forClass(Venue.class);
+        Mockito.verify(venueService).saveVenue(boundVenue.capture());
+
+        assertEquals(id, boundVenue.getValue().getId());
+        assertEquals(email, boundVenue.getValue().getEmail());
+        assertEquals(capacity, boundVenue.getValue().getCapacity());
+        assertEquals(password, boundVenue.getValue().getPassword());
+
+    }
+
+    @Test
+    public void testDelete() throws Exception{
+        Integer id = 1;
+        mockMvc.perform(get("/venue/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/venues"));
+
+        //check if venue service was called 1 time
+        Mockito.verify(venueService, Mockito.times(1)).deleteVenue(id);
+    }
 
 }
