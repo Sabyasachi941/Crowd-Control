d0203b39e1f70b3cdbec87efec4970eb612b581b
qharryq <harry.quigley2@mail.dcu.ie>
2016-05-21 17:25:39 +0100

    last commit b4 panic

--- ccwebapp/src/main/java/cc/WebSecurityConfig.java
+++ ccwebapp/src/main/java/cc/WebSecurityConfig.java
@@ -42,13 +42,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                //.and().httpBasic() is needed to curl to db but it makes annoying authentication pop up
                 //static refers to the static content - without this user would not be able to see images/styles/js effect w/out signing in!
                     .antMatchers("/", "/venues/new", "/static/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
-                .formLogin()
+                    .httpBasic();
+                http.formLogin()
                     .loginPage("/login")
-                    //.usernameParameter("email").passwordParameter("password")
                     .permitAll()
                     .and()
                 .logout()
